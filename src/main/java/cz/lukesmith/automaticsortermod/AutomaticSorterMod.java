9bda4b5b55b19ccb026b2f831ad3e57a27fbf870
EpicSniper <smith.lukas@seznam.cz>
2024-12-03 20:46:22 +0100

    Initial Commit

---
+++ src/main/java/cz/lukesmith/automaticsortermod/AutomaticSorterMod.java
@@ -0,0 +1,16 @@
+package cz.lukesmith.automaticsortermod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AutomaticSorterMod implements ModInitializer {
+	public static final String MOD_ID = "automatic-sorter-mod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/cz/lukesmith/automaticsortermod/AutomaticSorterModClient.java
@@ -0,0 +1,11 @@
+package cz.lukesmith.automaticsortermod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AutomaticSorterModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
