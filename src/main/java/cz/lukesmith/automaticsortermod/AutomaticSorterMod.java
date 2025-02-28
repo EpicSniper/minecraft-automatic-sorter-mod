fc17fa50f26c91aa83e70cdb3c78e2d2e0021870
Lukáš Smítal <smith.lukas@seznam.cz>
2025-02-28 02:03:39 +0100

    Renaming

--- src/main/java/cz/lukesmith/automaticsortermod/AutomaticSorterMod.java
+++ src/main/java/cz/lukesmith/automaticsortermod/AutomaticSorterMod.java
@@ -13,7 +13,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class AutomaticSorterMod implements ModInitializer {
-    public static final String MOD_ID = "automaticsortermod";
+    public static final String MOD_ID = "automaticsorter";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
@@ -25,7 +25,7 @@ public class AutomaticSorterMod implements ModInitializer {
 
         ModBlockEntities.registerModBlocksEntities();
         ModScreenHandlers.registerScreenHandlers();
-        ServerPlayNetworking.registerGlobalReceiver(new Identifier("automaticsortermod", "update_receive_items"), (server, player, handler, buf, responseSender) -> {
+        ServerPlayNetworking.registerGlobalReceiver(new Identifier(MOD_ID, "update_receive_items"), (server, player, handler, buf, responseSender) -> {
             int value = buf.readInt();
             server.execute(() -> {
                 if (player.currentScreenHandler instanceof FilterScreenHandler screenHandler) {
--- src/main/java/cz/lukesmith/automaticsortermod/item/ModItems.java
+++ src/main/java/cz/lukesmith/automaticsortermod/item/ModItems.java
@@ -1,7 +1,6 @@
 package cz.lukesmith.automaticsortermod.item;
 
 import cz.lukesmith.automaticsortermod.AutomaticSorterMod;
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
